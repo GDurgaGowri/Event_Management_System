e0d247957b602bb5740b4cc0067f6c726deffd96
Durgagowri G <125624840+GDurgaGowri@users.noreply.github.com>
2024-10-03 17:40:26 +0530

    Update Location.kt

--- src/main/kotlin/com/example/eventManagement/Entity/Location.kt
+++ src/main/kotlin/com/example/eventManagement/Entity/Location.kt
@@ -7,15 +7,15 @@ import jakarta.validation.constraints.NotNull
 @Table(name = "location")
 data class Location(
     @Id
-    val id: Long,  // Removed @GeneratedValue
+    val id: Long,  
 
     @field:NotNull
     @Column(name = "name", nullable = false)
     val name: String,
 
     @field:NotNull
-    @Column(name = "address") // Add this column
-    val address: String? = null, // Make it nullable or non-null as per your use case
+    @Column(name = "address") 
+    val address: String? = null, 
 
     @field:NotNull
     @OneToMany(mappedBy = "location", cascade = [CascadeType.ALL], orphanRemoval = true)
